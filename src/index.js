178a79e9ceceee0f4dc0097c35a1eba7bc8672f3
bilgicgurkan <bilgicgurkan44@gmail.com>
2023-05-26 20:12:56 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ const movieName = document.querySelector("#movieName");
 const movieDate = document.querySelector("#movieDate");
 const movieCategory = document.querySelector("#movieCategory");
 const movieDirector = document.querySelector("#movieDirector");
-const saveBtn = document.querySelector(".save")
+const saveBtn = document.querySelector(".save");
+const listArea = document.querySelector(".list-area");
+const updateMovieInfo = document.querySelector(".change-save");
 
 const request = new Request("http://localhost:3000/Movie");
 const ui = new UI();
+
 
 eventListeners();
 
 function eventListeners() {
     document.addEventListener("DOMContentLoaded", showItem);
     saveBtn.addEventListener("click", addNewMovie);
+    listArea.addEventListener("click", updateOrDelete);
+    updateMovieInfo.addEventListener("click", updateMovieInfos);
 }
 
 function showItem() {
@@ -43,4 +48,51 @@ function addNewMovie() {
     }
 
     ui.clearInput();
+}
+
+function updateOrDelete(e) {
+
+    if (e.target.className === "delete") {
+        deleteMovie(e.target);
+    }
+    if (e.target.className === "change") {
+        updateMovie(e.target.parentElement.parentElement);
+    }
+
+}
+
+function deleteMovie(target) {
+    const id = target.parentElement.previousElementSibling.firstElementChild.textContent.trim();
+    request.delete(id)
+        .then(deleteMovie => {
+            ui.deleteMovieFromUI(target.parentElement.parentElement)
+        })
+        .catch(err => console.log(err))
+}
+
+let updateState = null;
+
+function updateMovie(target) {
+    ui.changeButtonDisplay(target)
+
+    if (updateState === null) {
+        updateState = {
+            updateId: target.children[4].textContent.trim(),
+            updateParent: target
+        }
+    } else {
+        let updateState = null;
+    }
+}
+
+function updateMovieInfos() {
+
+
+    if (updateState) {
+        const data = { name: movieName.value.trim(), since: Number(movieDate.value.trim()), categories: movieCategory.value.trim(), director: movieDirector.value.trim() }
+        request.put(updateState.updateId, data)
+            .then()
+            .catch(err => console.log(err))
+    }
+
 }
--- src/ui.js
+++ src/ui.js
@@ -5,6 +5,7 @@ export class UI {
         this.movieDate = document.querySelector("#movieDate");
         this.movieCategory = document.querySelector("#movieCategory");
         this.movieDirector = document.querySelector("#movieDirector");
+        this.changeSave = document.querySelector(".change-save");
     }
     showAllMovie(movieData) {
         let result = "";
@@ -49,7 +50,14 @@ export class UI {
                     </div>
                 </div>
 
+                <div class="movie-info">
+                    <div class="id" style="display:none">
+                        ${movie.id}
+                    </div>
+                </div>
+
                 <div class="button-area">
+                    <button class="delete">Sil</button>
                     <button class="change">Düzenle</button>
                 </div>
 
@@ -59,10 +67,41 @@ export class UI {
 
         this.listArea.innerHTML = result;
     }
-    clearInput(){
+    clearInput() {
         this.movieName.value = "";
         this.movieDate.value = "";
         this.movieCategory.value = "";
         this.movieDirector.value = "";
+    }
+    deleteMovieFromUI(target) {
+        target.remove();
+    }
+    changeButtonDisplay(target) {
+        if (this.changeSave.style.display === "none") {
+            this.changeSave.style.display = "block"
+            this.addMovieInfoToInput(target);
+            this.changeButtonHTML(target);
+        } else {
+            this.changeSave.style.display = "none"
+            this.clearInput();
+            this.changeButtonHTML(target);
+        }
+    }
+    addMovieInfoToInput(target) {
+        const children = target.children;
+
+        this.movieName.value = children[0].lastElementChild.textContent.trim();
+        this.movieDate.value = children[1].lastElementChild.textContent.trim();
+        this.movieCategory.value = children[2].lastElementChild.textContent.trim();
+        this.movieDirector.value = children[3].lastElementChild.textContent.trim();
+
+    }
+    changeButtonHTML(e) {
+        if (this.changeSave.style.display === "block") {
+            e.lastElementChild.lastElementChild.innerHTML = "Geri Al"
+        }
+        else if (this.changeSave.style.display === "none") {
+            e.lastElementChild.lastElementChild.innerHTML = "Düzenle"
+        }
     }
 }
